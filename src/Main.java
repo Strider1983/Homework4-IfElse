bcb46d313a77e26231087d1d24b77edc817ddc55
Konstantin <mezentsev.k@gmail.com>
2023-08-11 21:31:32 +0800

    Задача №1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Условные операторы");
+        System.out.println("----------");
+        System.out.println("Задача №1");
+        int age = 13;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
